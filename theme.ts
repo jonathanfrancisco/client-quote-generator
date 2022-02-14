6729d60e4bb6ff0efdc5899694da13c1b8e0654c
Jonathan Francisco <thinklikejo19@gmail.com>
2022-02-14 23:25:54 +0800

    add accent color

--- theme.ts
+++ theme.ts
@@ -2,6 +2,7 @@ const theme = {
   Colors: {
     primary: '#FFCB03',
     secondary: 'FCF9EE',
+    accent: '#FDEEB3',
     btnPrimary: '#1F4B67',
     btnSecondary: '#E9E9E0',
     txtPrimary: '#000000',
