5bc77fc3057f8bdab5bf5323a851564ea27473d6
Jonathan Francisco <thinklikejo19@gmail.com>
2022-10-16 03:48:14 +0800

    Fix dashboard fonts

--- src/components/Dashboard/ActionCard.tsx
+++ src/components/Dashboard/ActionCard.tsx
@@ -22,12 +22,10 @@ const ActionCard = ({ icon, text, onPress }: ActionCardProps) => {
       <TouchableOpacity
         onPress={onPress}
         style={tw`w-1/2.3 h-48 shadow-md bg-white mt-5 py-18 justify-center items-center rounded-xl`}>
-        <View style={tw`p-3 bg-sunlife-accent rounded-full text-black`}>
+        <View style={tw`p-3 mb-2 bg-sunlife-accent rounded-full text-black`}>
           <Image source={icon} />
         </View>
-        <Text style={tw`font-thin text-center text-base text-black`}>
-          {text}
-        </Text>
+        <Text style={tw`text-center text-base text-black`}>{text}</Text>
       </TouchableOpacity>
     );
   }
@@ -36,7 +34,7 @@ const ActionCard = ({ icon, text, onPress }: ActionCardProps) => {
     <TouchableOpacity
       onPress={onPress}
       style={tw`w-1/2.3 h-48 shadow-md bg-white mt-5 py-18 justify-center items-center rounded-xl`}>
-      <Text style={tw`font-thin text-center text-lg text-black`}>{text}</Text>
+      <Text style={tw`text-center text-lg text-black`}>{text}</Text>
     </TouchableOpacity>
   );
 };
--- src/components/Dashboard/QuoteCount.tsx
+++ src/components/Dashboard/QuoteCount.tsx
@@ -9,7 +9,7 @@ type Props = {
 const QuoteCount = ({ count }: Props) => {
   return (
     <View style={tw`items-end px-8 pb-8`}>
-      <Text style={tw`text-white font-bold text-5xl`}>{count}</Text>
+      <Text style={tw`text-white font-extrabold text-6xl`}>{count}</Text>
       <Text style={tw`font-bold text-sm text-black`}>Total Quote</Text>
     </View>
   );
