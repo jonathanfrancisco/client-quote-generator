afc37e66a42dc5352ba45da1f512d4b8c564ad9e
Jonathan Francisco <thinklikejo19@gmail.com>
2022-11-04 00:59:59 +0800

    remove primary and supplementray benefits validation temporarily for todo

--- src/formSchemas/clientBenefitsStep.schema.ts
+++ src/formSchemas/clientBenefitsStep.schema.ts
@@ -1,27 +1,43 @@
 import * as Yup from 'yup';
 
-const benefitsValidationSchemaRule = Yup.array()
-  .of(
-    Yup.object().shape({
-      isSelected: Yup.boolean(),
-      id: Yup.string(),
-      type: Yup.string(),
-      name: Yup.string(),
-      amount: Yup.boolean(),
-      value: Yup.string(),
-    })
-  )
-  .test({
-    message: 'Must select atleast 1 benefit',
-    test: (arr) => {
-      const selectedBenefits = arr?.filter((i) => i.isSelected).length || 0;
-      return selectedBenefits >= 1;
-    },
-  });
-
 const ClientBenefitsStepSchema = Yup.object().shape({
-  productPrimaBenefits: benefitsValidationSchemaRule,
-  productSuppBenefits: benefitsValidationSchemaRule,
+  // TODO: Readd validation
+  // productPrimaBenefits: Yup.array()
+  //   .of(
+  //     Yup.object().shape({
+  //       isSelected: Yup.boolean(),
+  //       id: Yup.string(),
+  //       type: Yup.string(),
+  //       name: Yup.string(),
+  //       amount: Yup.boolean(),
+  //       value: Yup.string(),
+  //     })
+  //   )
+  //   .test({
+  //     message: 'Must select atleast 1 benefit',
+  //     test: (arr) => {
+  //       const selectedBenefits = arr?.filter((i) => i.isSelected).length || 0;
+  //       return selectedBenefits >= 1;
+  //     },
+  //   }),
+  // productSuppBenefits: Yup.array()
+  //   .of(
+  //     Yup.object().shape({
+  //       isSelected: Yup.boolean(),
+  //       id: Yup.string(),
+  //       type: Yup.string(),
+  //       name: Yup.string(),
+  //       amount: Yup.boolean(),
+  //       value: Yup.string(),
+  //     })
+  //   )
+  //   .test({
+  //     message: 'Must select atleast 1 benefit',
+  //     test: (arr) => {
+  //       const selectedBenefits = arr?.filter((i) => i.isSelected).length || 0;
+  //       return selectedBenefits >= 1;
+  //     },
+  //   }),
 });
 
 export default ClientBenefitsStepSchema;
